378c93bdda4960680cb83f344d503fcbb01d744d
liu <1476879092@qq.com>
2021-03-10 19:13:50 +0800

    3.5

--- about.py
+++ about.py
@@ -20,7 +20,7 @@ class About (AboutGui):
         self.about_txt.BeginBold()
         self.about_txt.WriteText("Version: ")
         self.about_txt.EndBold()
-        self.about_txt.WriteText("3.4\n")
+        self.about_txt.WriteText("3.5\n")
 
         self.about_txt.BeginBold()
         self.about_txt.WriteText("Platform: ")

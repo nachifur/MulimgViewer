d098ee8ecbd403ecae01f53396bcf7da62d85de1
nachifur <1476879092@qq.com>
2022-01-17 17:28:04 +0800

    update code

--- main_gui.py
+++ main_gui.py
@@ -185,7 +185,7 @@ class MulimgViewerGui ( wx.Frame ):
 
 		image_interpChoices = [ u"Nearest", u"Cubic", u"Linear" ]
 		self.image_interp = wx.Choice( self.scrolledWindow_set, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, image_interpChoices, 0 )
-		self.image_interp.SetSelection( 2 )
+		self.image_interp.SetSelection( 0 )
 		bSizer23.Add( self.image_interp, 0, wx.ALL, 5 )
 
 
@@ -446,7 +446,7 @@ class MulimgViewerGui ( wx.Frame ):
 
 		wSizer6.Add( self.m_staticText6, 1, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
 
-		self.show_scale = wx.TextCtrl( self.scrolledWindow_set, wx.ID_ANY, u"1,1", wx.DefaultPosition, wx.Size( 60,-1 ), style=wx.TE_PROCESS_ENTER )
+		self.show_scale = wx.TextCtrl( self.scrolledWindow_set, wx.ID_ANY, u"1,1", wx.DefaultPosition, wx.Size( 60,-1 ), 0 )
 		wSizer6.Add( self.show_scale, 0, wx.ALL, 5 )
 
 		self.m_staticText7 = wx.StaticText( self.scrolledWindow_set, wx.ID_ANY, u"Out", wx.DefaultPosition, wx.DefaultSize, 0 )

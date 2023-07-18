8deeecf564831839435ac0706b2c6e5b3a69e8f2
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-07-18 19:31:46 +0800

    :bug: Fix version of flake.nix

--- flake.nix
+++ flake.nix
@@ -11,8 +11,7 @@
         {
           formatter = nixpkgs-fmt;
           packages.default = buildPythonApplication rec {
-            pname = "mulimgviewer";
-            version = "";
+            name = "mulimgviewer";
             src = self;
             format = "pyproject";
             disabled = pythonOlder "3.6";

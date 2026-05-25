2eb37df049e8ad39396cc4aa8e1510bf200bc6cb
陈映赫 <2205774782@qq.com>
2026-05-25 14:03:13 +0800

    修复自定义算法刷新并统一保存目录
    问题：CustomFunc 下拉框添加或删除算法后，界面列表和 Python 模块缓存可能不同步；同时选中算法后的处理结果、拼接图、放大镜图和选中图片保存目录不统一，容易写到错误位置。
    
    修改：删除不应该存在的 Oil Painting Effect，添加算法时校验路径、main.py 和重复名称，添加或删除后清理模块缓存并刷新下拉框；同时把选中算法相关输出统一保存到 processing_function/<算法名>/ 下。
    
    范围：只影响 CustomFunc 下拉框、添加/删除算法、算法列表刷新，以及选中 CustomFunc 算法后的保存路径。

--- src/mulimgviewer/src/custom_func/Gaussian Blur/main.py
+++ src/mulimgviewer/src/custom_func/Gaussian Blur/main.py
@@ -14,33 +14,21 @@ def main(img_list, save_path, name_list=None, algorithm_name="Gaussian Blur"):
     out_img_list = []
     if save_path != "":
         flag_save = True
-        base_save_path = Path(save_path) / "processing_function" / algorithm_name / "processed_img"
-        if not base_save_path.exists():
-            os.makedirs(str(base_save_path))
+        save_path = Path(save_path) / "processing_function" / algorithm_name / "processed_img"
+        if not save_path.exists():
+            os.makedirs(str(save_path))
     else:
         flag_save = False
-        base_save_path = None
 
     for img in img_list:
         img = custom_process_img(img)
+
         out_img_list.append(img)
         if flag_save:
             if isinstance(name_list, list) and i < len(name_list):
-                name = name_list[i]
-                name_path = Path(name)
-                if len(name_path.parts) > 1:
-                    sub_folder = name_path.parent
-                    file_name = name_path.name
-                    final_save_path = base_save_path / sub_folder
-                else:
-                    final_save_path = base_save_path
-                    file_name = name
-
-                if not final_save_path.exists():
-                    os.makedirs(str(final_save_path))
-                img_path = final_save_path / file_name
+                img_path = save_path / name_list[i]
             else:
-                img_path = base_save_path / (str(i) + ".png")
+                img_path = save_path / (str(i) + ".png")
             img.save(str(img_path))
         i += 1
     return out_img_list
